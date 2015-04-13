8e98b36c1931d8124c2060c46c441e77d4af683b
Felix <felix@ingenos-MacBook-Pro.local>
2015-04-13 10:02:22 +0200

    cleanup

--- grunt/contrib-uglify.js
+++ grunt/contrib-uglify.js
@@ -10,7 +10,6 @@ module.exports = function(grunt) {
           files: {
               'generated/js/modernizr.custom.js': ['app/js/modernizr.custom.js'],
               'generated/js/toucheffects.js': ['app/js/toucheffects.js'],
-              'generated/js/iframeResizer.contentWindow.min.js': ['app/js/iframeResizer.contentWindow.min.js'],
               'generated/js/jquery.js': ['bower_components/jquery/dist/jquery.js'],
               'generated/js/bootstrap.min.js': ['app/js/bootstrap.min.js'],
               'generated/js/functions.js': ['app/js/functions.js']
@@ -27,7 +26,6 @@ module.exports = function(grunt) {
           files: {
               'dist/js/modernizr.custom.js': ['app/js/modernizr.custom.js'],
               'dist/js/toucheffects.js': ['app/js/toucheffects.js'],
-              'dist/js/iframeResizer.contentWindow.min.js': ['app/js/iframeResizer.contentWindow.min.js'],
               'dist/js/jquery.js': ['bower_components/jquery/dist/jquery.js'],
               'dist/js/bootstrap.min.js': ['app/js/bootstrap.min.js'],
               'dist/js/functions.js': ['app/js/functions.js']
